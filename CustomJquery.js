724d4e5c913095fbfcca2595e0cd646c22fc8a34
Mahesh Pawate <mahesh@circle.news>
2020-07-02 20:55:10 +0530

    all updated

--- CustomJquery.js
+++ CustomJquery.js
@@ -10,6 +10,42 @@ Jquery.prototype.each = function(callback){
     return this;
 }
 
+Jquery.prototype.hide=function(){
+    return displayToggle.call(this, this.nodes)
+}
+
+Jquery.prototype.show=function(){
+    return displayToggle.call(this,this.nodes,true)
+}
+
+function fadIn(){
+    // setTimeout(3000){
+        // show('msg')
+    // }
+}
+
+function fadOut(){
+    // setTimeout(3000)
+}
+
+function displayToggle(elements, flag){
+    for(let i=0;i<elements.length;i++){
+        let item=elements[i]
+        let display=item.style.display
+        if(flag){
+            if(display==="none"){
+                item.style.display="block"
+            }
+        }
+        else{
+            if(display!=="none"){
+                item.style.display="none"
+            }
+        }
+    }
+    return this
+}
+
 function $( selector ){
     return new Jquery(selector)
 }
